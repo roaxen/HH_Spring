eda15bcbf41fbc5c2e6bdb2dc9c610c754079af8
Pablo Javier Casado Marco <pablojcm02@gmail.com>
2023-04-21 17:55:02 +0200

    usuario blob

--- HomelessHelper/HomelessHelper/src/main/java/model/Usuario.java
+++ HomelessHelper/HomelessHelper/src/main/java/model/Usuario.java
@@ -3,40 +3,36 @@ package model;
 import java.io.Serializable;
 import jakarta.persistence.*;
 import java.util.Date;
-import java.util.List;
+
 
 /**
  * The persistent class for the usuario database table.
  * 
  */
 @Entity
-@NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u")
+@NamedQuery(name="Usuario.findAll", query="SELECT u FROM Usuario u")
 public class Usuario implements Serializable {
-	@Override
-	public String toString() {
-		return "Usuario [email=" + email + ", apellidos=" + apellidos + ", ciudad=" + ciudad + ", clave=" + clave
-				+ ", fechaNac=" + fechaNac + ", nombre=" + nombre + ", lugars=" + lugars + "]";
-	}
-
 	private static final long serialVersionUID = 1L;
+
 	@Id
 	private String email;
+
 	private String apellidos;
+
 	private String ciudad;
+
 	private String clave;
+
 	@Temporal(TemporalType.DATE)
-	@Column(name = "fecha_nac")
+	@Column(name="fecha_nac")
 	private Date fechaNac;
+
+	@Lob
+	private byte[] imagen;
+
 	private String nombre;
-	// bi-directional many-to-one association to Lugar
-	@OneToMany(mappedBy = "emailResponsable")
-	private List<Lugar> lugars;
 
 	public Usuario() {
-	}
-
-	public Usuario(String email) {
-		this.email = email;
 	}
 
 	public String getEmail() {
@@ -79,21 +75,20 @@ public class Usuario implements Serializable {
 		this.fechaNac = fechaNac;
 	}
 
+	public byte[] getImagen() {
+		return this.imagen;
+	}
+
+	public void setImagen(byte[] imagen) {
+		this.imagen = imagen;
+	}
+
 	public String getNombre() {
 		return this.nombre;
 	}
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
-	}
-	
-
-	public List<Lugar> getLugars() {
-		return this.lugars;
-	}
-
-	public void setLugars(List<Lugar> lugars) {
-		this.lugars = lugars;
 	}
 
 }
